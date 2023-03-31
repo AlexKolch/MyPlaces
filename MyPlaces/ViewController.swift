c9202e3f67d37a11bf0f5ebbf44bf3aab416102a
Alexey Kolch <kolychenkov94@mail.ru>
2023-03-31 09:50:07 +0400

    app icon
    App icon
    Launch Screen
    Row height

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -28,11 +28,15 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
 
-
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
 }
 
