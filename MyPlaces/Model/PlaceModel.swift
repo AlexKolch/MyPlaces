37f48898daeff0695937c74a58007313042cc759
Alexey Kolch <kolychenkov94@mail.ru>
2023-04-02 20:35:35 +0400

    Revert "save in dataBase"
    This reverts commit 9800bb4d59fb047df95cc606fcabbd3ae240fe75.

--- MyPlaces/Model/PlaceModel.swift
+++ MyPlaces/Model/PlaceModel.swift
@@ -4,36 +4,27 @@
 //
 //  Created by Алексей Колыченков on 31.03.2023.
 //
-import Foundation
-import RealmSwift
+
 import UIKit
 
-class Place: Object {
+struct Place {
 
-    @objc dynamic var name = ""
-    @objc dynamic var location: String?
-    @objc dynamic var type: String?
-    @objc dynamic var imageData: Data?
+    var name: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
 
-
-     let restaurantNames = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Дастархан", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy"]
+    static let restaurantNames = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Дастархан", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy"]
 
 //метод получения массива [Place] для отображения в cell
-    func savePlaces() {
+    static func getPlaces() -> [Place] {
+        var places = [Place]()
 
         for place in restaurantNames {
-            let image = UIImage(named: place)
-            guard let imageData = image?.pngData() else {return}
-
-            let newPlace = Place()
-
-            newPlace.name = place
-            newPlace.location = "Санкт-Петербург"
-            newPlace.type = "Ресторан"
-            newPlace.imageData = imageData
-
-            StorageManager.saveObject(newPlace)
+            places.append(Place(name: place, location: "Санкт-Петербург", type: "Ресторан", restaurantImage: place))
         }
 
+        return places
     }
 }
