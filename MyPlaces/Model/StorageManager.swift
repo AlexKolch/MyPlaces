90b7f736c65ae7efc5c3b1a609aef88be5dbd71c
Alexey Kolch <kolychenkov94@mail.ru>
2023-04-06 15:46:53 +0400

    Merge Cosmos SDK (#5)
    * Merge Database (#1)
    
    * add Realm
    
    * save in dataBase
    
    * Revert "save in dataBase"
    
    This reverts commit 9800bb4d59fb047df95cc606fcabbd3ae240fe75.
    
    * Save in database
    
    * Delete from database
    
    * setupEditScreen
    
    * setupEditScreen Done
    
    * Search is Database branch Done (#3)
    
    * add Realm
    
    * save in dataBase
    
    * Revert "save in dataBase"
    
    This reverts commit 9800bb4d59fb047df95cc606fcabbd3ae240fe75.
    
    * Save in database
    
    * Delete from database
    
    * setupEditScreen
    
    * setupEditScreen Done
    
    * Merge Sorted (#2)
    
    * SegmentControl add
    
    * migrationBlock
    
    Manually Migrate Schema add
    
    * sorting Done!
    
    * Search Done
    
    - UISearchController
    - updateSearchResults
    - filteredPlaces
    - bug filtered prepareForSegue fix
    
    * Merge is DataBase  (#4)
    
    * add Realm
    
    * save in dataBase
    
    * Revert "save in dataBase"
    
    This reverts commit 9800bb4d59fb047df95cc606fcabbd3ae240fe75.
    
    * Save in database
    
    * Delete from database
    
    * setupEditScreen
    
    * setupEditScreen Done
    
    * Merge Sorted (#2)
    
    * SegmentControl add
    
    * migrationBlock
    
    Manually Migrate Schema add
    
    * sorting Done!
    
    * Search Done
    
    - UISearchController
    - updateSearchResults
    - filteredPlaces
    - bug filtered prepareForSegue fix
    
    * rating stars done
    
    * rating stars save in database Done
    
    * CocoaPods install
    
    Cosmos frameworks
    
    * Cosmos SDK Done
    
    - Cosmos SDK
    - Code Review

---
+++ MyPlaces/Model/StorageManager.swift
@@ -0,0 +1,25 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Алексей Колыченков on 02.04.2023.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+
+   static func saveObject(_ place: Place) {
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+
+    static func deleteObject(_ place: Place) {
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+}
